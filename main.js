de70ec56f14401af7623330fe1eb6e647b094079
aswinblix <aswin.blix@bitsnpixs.com>
2024-04-23 10:16:55 +0530

    End of Lesson 1

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            description: 'Made with 100% cotton.'
+        }
+    }
+})
